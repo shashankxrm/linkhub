01ac7ead45fb6b51789a0da6b68ca2125aa5468a
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-01-09 14:28:01 +0530

    added  vercel anal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Tree from "./components/Tree";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div>
       <Tree />
+      <Analytics />
     </div>
   );
 }
